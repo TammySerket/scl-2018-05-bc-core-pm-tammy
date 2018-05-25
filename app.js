e5d02559e74088b370b0cef8b068e3d209f93234
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-05-25 11:46:54 -0400

    Agregué formato de CSS

--- app.js
+++
@@ -1,9 +0,0 @@
-const abc = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-/*partes de for*Inicialización*/;/*condición*/;/*iteración*/
-for(let index = 0; index < abc.length; index++){
-    ("Letra "+
-    (index+1)+
-    " del ABCdario : "+
-abc.charAt(index)+
-"/n");
-}
