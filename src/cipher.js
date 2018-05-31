00c823e2a75e6a0b41a9e01a0887854f525c30ee
Tamara Hinojosa <tamara.hi.sa@hotmail.com>
2018-05-31 16:47:49 -0400

    agregando formato a cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,39 @@
 /* no se utiliza this en cipher*/
-window.cipher = {
-  encode: ()=> (offset) {
+/*const cipher =(string.toUpperCase){
+  cipher.encode = "";
+  cipher.decode = "";
+  window.cipher.encode = "";
+  window.cipher.decode = "";
+  
+function createCipherWithOffset():  
+cipher.createCipherWithOffset()
+}
+let abc = "abcdefghijklmnñopqrstuvwxyz" {
+
+}
+  
+
+window.cipher =  {
+  encode: (: )=> () {
     /* Acá va tu código (la flecha reemplaza a function)*/
-    cipher.encode(string, offset)
-  },
+   /* cipher.encode()
+  }
   decode: () => {
     /* Acá va tu código */
-    cipher.decode()
-  }
+  /*  cipher.decode()
+  } 
 }
-window.cipher.decode(;)
+window.cipher.decode(;) */
+
+cipherencode:(abc, offset) => { //dando nombre a la funcion
+  var textoacifrar1 = abc.toUpperCase();
+  var textoacifrar2 = "";
+  console.log(textoacifrar1)
+  for (i = 0; i < textoacifrar1.length; i++) { //busca codigo ASCII y guarda la letra en textoacifrar1
+
+    var offset = textoacifrar1.charCodeAt(i);
+    if (offset == 35) 
+  }
+  textoacifrar2 = textoacifrar2 + string.charCodeAt(offset); //guarda el espacio
+ 
+}
