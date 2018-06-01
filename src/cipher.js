c9c6d8bf6262f0963df2b03c0bff7f5b9bb57565
Tamara Hinojosa <tamara.hi.sa@hotmail.com>
2018-06-01 01:14:40 -0400

    sigo arreglando cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,13 @@
-/* no se utiliza this en cipher*/
-/*const cipher =(string.toUpperCase){
-  cipher.encode = "";
-  cipher.decode = "";
-  window.cipher.encode = "";
-  window.cipher.decode = "";
-  
-function createCipherWithOffset():  
-cipher.createCipherWithOffset()
-}
-let abc = "abcdefghijklmnñopqrstuvwxyz" {
-
-}
-  
-
-window.cipher =  {
-  encode: (: )=> () {
-    /* Acá va tu código (la flecha reemplaza a function)*/
-   /* cipher.encode()
+window.cipher =  { /* Acá va tu código (la flecha reemplaza a function)*/
+  encode: (abc, offset ) => {//dando nombre a la funcion
+   abc = document.getElementById("msjacifrar").value;
+  let textoacifrar1 = "";
   }
-  decode: () => {
-    /* Acá va tu código */
-  /*  cipher.decode()
-  } 
-}
-window.cipher.decode(;) */
-
-cipherencode:(abc, offset) => { //dando nombre a la funcion
-  var textoacifrar1 = abc.toUpperCase();
-  var textoacifrar2 = "";
-  console.log(textoacifrar1)
-  for (i = 0; i < textoacifrar1.length; i++) { //busca codigo ASCII y guarda la letra en textoacifrar1
-
-    var offset = textoacifrar1.charCodeAt(i);
-    if (offset == 35) 
+  
+  for (let i = 0; i < textoacifrar1.length; i++) { //busca codigo ASCII y guarda la letra en textoacifrar1
+    const formula = (abc.toUpperCase().charCodeAt(i)- 65 + offset)%26 + 65;
   }
-  textoacifrar2 = textoacifrar2 + string.charCodeAt(offset); //guarda el espacio
- 
+decode: (abc, offset) => {
+  /* Acá va tu código */
 }
+
