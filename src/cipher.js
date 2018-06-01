3208c8e1e71342d392798b11db6af21a671b4cc3
Tamara Hinojosa <tamara.hi.sa@hotmail.com>
2018-06-01 15:28:00 -0400

    Se intenta arreglar una ultima vez con ayuda de compañera

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,22 @@
 window.cipher =  { /* Acá va tu código (la flecha reemplaza a function)*/
   encode: (mensaje, offset ) => {//dando nombre a la funcion
    mensaje = document.getElementById("msjacifrar").value;
-   offset = parseInt(document.getElementById('key').value);
-  let textoCifrar = "";
+   offset = (document.getElementById('key'.parseIn()).value);
   for (let i = 0; i < textoCifrar.length; i++) { //busca codigo ASCII y guarda la letra en textoacifrar1
-    const cipher = (mensaje.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;
-    const offset = String.fromCharCode(cipher);
+    let ciphator = (mensaje.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;
+    let offset = String.fromCharCode(ciphator);
     textoCifrar+=cipher;
   }
- document.getElementById("cajaResultado").innerHTML = textoCifrar
-  }
+ //document.getElementById("cajaResultado").innerHTML = textoCifrar
+  },
 
   decode: (mensaje, offset) => {
   /* Acá va tu código */
   mensaje = document.getElementById("msjcifrado").value;
   offset = parseInt(document.getElementById('key').value);
-  let textoDescifrado = "";
   for (let i = 0; i < textoDescifrado.length; i++) { //busca codigo ASCII y guarda la letra en textoacifrar1
-    const cipher = (mensaje.toUpperCase().charCodeAt(i) + 65 - offset) % 26 + 65;
-    const offset = String.fromCharCode(cipher);
+    let ciphator = (mensaje.toUpperCase().charCodeAt(i) + 65 - offset) % 26 + 65;
+    let offset = String.fromCharCode(ciphator);
     textoDescifrado+=cipher;
   }
   document.getElementById("cajaResultado").innerHTML = textoDescifrado
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-function encode(){
-    let mensaje = document.getElementById("msjacifrar").value;
-    let offset = parseInt(document.getElementById("key").value);
-    const resultado = window.cipher.encode(mensaje, offset);
-    document.getElementById("cajaResultado").innerHTML = resultado;
+function encode(mensaje, offset){
+    document.getElementById("msjacifrar").value, document.getElementById("key".parseInt().value);
+    var resultado = document.getElementById(cipher.encode()).value;
+    document.getElementById("cajaResultado").value = resultado;
+    //let mensaje = document.getElementById("btn1").value;
+    //let offset = parseInt(document.getElementById("key").value);
+    //const resultado = encode(mensaje, offset);
+    //document.getElementById("cajaResultado").innerHTML = resultado;
 }
 
 function decode(){
-    let mensaje = document.getElementById("msjcifrado").value;
+    let mensaje = document.getElementById("btn2").value;
     let offset = parseInt(document.getElementById("key").value);
-    const resultado = window.cipher.decode(mensaje, offset);
+    const resultado = decode(mensaje, offset);
     document.getElementById("cajaResultado").innerHTML = resultado;
 }
