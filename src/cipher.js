45de914be2ae51945dcb3654a036fa5188096e3c
Tamara Hinojosa <tamara.hi.sa@hotmail.com>
2018-06-01 11:41:57 -0400

    cambié buttons por input porque no funcionaba, pero sigue sin funcionar u.u

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,27 @@
 window.cipher =  { /* Acá va tu código (la flecha reemplaza a function)*/
-  encode: (abc, offset ) => {//dando nombre a la funcion
-   abc = document.getElementById("msjacifrar").value;
-  let textoacifrar1 = "";
+  encode: (mensaje, offset ) => {//dando nombre a la funcion
+   mensaje = document.getElementById("msjacifrar").value;
+   offset = parseInt(document.getElementById('key').value);
+  let textoCifrar = "";
+  for (let i = 0; i < textoCifrar.length; i++) { //busca codigo ASCII y guarda la letra en textoacifrar1
+    const cipher = (mensaje.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;
+    const offset = String.fromCharCode(cipher);
+    textoCifrar+=cipher;
   }
-  
-  for (let i = 0; i < textoacifrar1.length; i++) { //busca codigo ASCII y guarda la letra en textoacifrar1
-    const formula = (abc.toUpperCase().charCodeAt(i)- 65 + offset)%26 + 65;
+ document.getElementById("cajaResultado").innerHTML = textoCifrar
   }
-decode: (abc, offset) => {
+
+decode: (mensaje, offset) => {
   /* Acá va tu código */
+  mensaje = document.getElementById("msjcifrado").value;
+  offset = parseInt(document.getElementById('key').value);
+  let textoDescifrado = "";
+  for (let i = 0; i < textoDescifrado.length; i++) { //busca codigo ASCII y guarda la letra en textoacifrar1
+    const cipher = (mensaje.toUpperCase().charCodeAt(i) + 65 - offset) % 26 + 65;
+    const offset = String.fromCharCode(cipher);
+    textoDescifrado+=cipher;
+  }
+  document.getElementById("cajaResultado").innerHTML = textoDescifrado
+}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-cipher.encode()
-cipher.decode()
+function encode(){
+    let mensaje = document.getElementById("msjacifrar").value;
+    let offset = parseInt(document.getElementById("key").value);
+    const resultado = window.cipher.encode(mensaje, offset);
+    document.getElementById("cajaResultado").innerHTML = resultado;
+}
