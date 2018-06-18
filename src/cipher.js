7689f82bd0d621831da0a2f1365e5ce78504db71
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-06-18 03:16:43 -0400

    ahora da undefined u.u

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,17 @@
 window.cipher = {
 
   encode: (mensaje, offset) => {
-  let answer1 = ""; //variable para el mensaje guardado
+  
    for (let i = 0; i < mensaje.length; i++) { //busca codigo ASCII y guarda la letra en msjacifrar
     const cifrar = (mensaje.charCodeAt(i)- 65 + offset) % 26 + 65;
     answer1 = String.fromCharCode(cifrar);
   }
   const btnEncode = document.getElementById("btn1");
     btnEncode.addEventListener("click", ()=>{      
-      document.getElementById("result").value = answer1;
+      let answer1 = ""; //variable para el mensaje guardado al hacer click
+      answer1 = document.getElementById("msjacifrar").value;
+      let offset = document.getElementById("key").value;
+       document.getElementById("result").innerHTML =  window.cipher.encode(answer1, offset);
     })
    
    },
