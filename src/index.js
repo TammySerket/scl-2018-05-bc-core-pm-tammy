924c5ba6caba3d1e3979eaab3e1603b5983bf443
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-06-19 23:15:06 -0400

    con ayuda de compañera ya logra cifrar y descifrar, se agregan últimos comentarios

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 function encode() {
-    const mensaje = document.getElementById("msjacifrar").value;
-    const llave = document.getElementById("key").value;    
-    const answer = window.cipher.encode(mensaje,llave);   
-    document.getElementById("result").innerHTML = answer;
+    const mensaje = document.getElementById("msjacifrar").value;//se crea constante con valor de mensaje
+    const llave = document.getElementById("key").value;// se crea constante con valor de llave    
+    const answer = window.cipher.encode(mensaje,llave);// se crea constante answer que llama a mensaje y llave de cipher.js   
+    document.getElementById("result").innerHTML = answer;//se llama a result para que tenga el valor de answer y de el resultado
     }
 
 
 function decode() {
-    const mensaje2 = document.getElementById("msjcifrado").value;
-    const llave = parseInt(document.getElementById("key").value);
-    const answer = window.cipher.decode(mensaje2,llave);
-    document.getElementById("result").innerHTML = answer;
-  /*  const btnDecode = document.getElementById("btn2");
-    btnDecode.addEventListener("click", ()=>{      
-    })*/
+    const mensaje2 = document.getElementById("msjcifrado").value;//se crea constante con valor de mensaje
+    const llave = parseInt(document.getElementById("key").value);// se crea constante con valor de llave
+    const answer = window.cipher.decode(mensaje2,llave);// se crea constante answer que llama a mensaje y llave de cipher.js 
+    document.getElementById("result").innerHTML = answer;//se llama a result para que tenga el valor de answer y de el resultado
 }
 
