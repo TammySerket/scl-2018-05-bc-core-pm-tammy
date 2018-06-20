1e7e106c65f7f27781458b3c2e550f1d684a7a47
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-06-19 23:21:22 -0400

    no logra descifrar las letras WXYZ

--- src/cipher.js
+++ src/cipher.js
@@ -3,7 +3,7 @@ window.cipher = {
   encode: (mensaje, llave) => {
     let answer1 = ""; //variable para el mensaje guardado al hacer click
     let offset = parseInt(llave); //se crea variable local de llave con parseint 
-    for (let i = 0; i <= mensaje.length; i++) { //busca codigo ASCII y guarda la letra en msjacifrar
+    for (let i = 0; i < mensaje.length; i++) { //busca codigo ASCII y guarda la letra en msjacifrar
       let msjacifrar = (mensaje.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;// se crea variable de mensaje para almacenar en numeros
       let msjcifrado = String.fromCharCode(msjacifrar);// se crea variable que pasa el cifrado de numeros a string
 
@@ -17,7 +17,7 @@ window.cipher = {
   decode: (mensaje2, llave) => {
     let answer1 = "";//variable para el mensaje guardado al hacer click
     let offset = parseInt(llave);//se crea variable local de llave con parseint 
-    for (let i = 0; i <= mensaje2.length; i++) { //busca codigo ASCII y guarda la letra en msjacifrar
+    for (let i = 0; i < mensaje2.length; i++) { //busca codigo ASCII y guarda la letra en msjacifrar
       let msjacifrar = (mensaje2.toUpperCase().charCodeAt(i) - 65 - offset) % 26 + 65;// se crea variable de mensaje para almacenar en numeros
       let msjcifrado = String.fromCharCode(msjacifrar);// se crea variable que pasa el cifrado de numeros a string
 
